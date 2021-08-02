13dff4c2ac98683e6a2f38bdfcaebe4d2176845c
Ruslan73region <82434875+Ruslan73region@users.noreply.github.com>
2021-08-02 11:13:59 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+const DATA = [
+  { id: "todo-0", name: "Football", completed: true },
+  { id: "todo-1", name: "Car", completed: false },
+  { id: "todo-2", name: "Work", completed: false }
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
